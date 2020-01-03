fc73057fe5c8cce27a953125e5a518e7a1143e09
CXMsanjay <sanjaykumarjass4@gmail.com>
2020-01-03 14:29:08 +0530

    2.Practise_Added

--- 2.practise/guestbook/urls.py
+++ 2.practise/guestbook/urls.py
@@ -1,21 +1,14 @@
-"""guestbook URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('myadmin/', admin.site.urls),
+    path('home/<stage>' , views.f2 ),
+    path('index/<name>' , views.f3 ),
+    path('info/<name>/<no>' , views.f4 ),
+    path('page/<name>/<int:no>' , views.f5 ),
+    path('next/<str:name>/<int:no>', views.f6  ),
+    path('new/<slug:idno>', views.f7)    
 ]
