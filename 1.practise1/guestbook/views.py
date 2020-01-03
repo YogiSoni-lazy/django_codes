33a9af9ead01d059a4805732eaa0a8243617e636
CXMsanjay <sanjaykumarjass4@gmail.com>
2020-01-03 14:00:00 +0530

    practise1_finish

--- 1.practise1/guestbook/views.py
+++ 1.practise1/guestbook/views.py
@@ -1,3 +1,4 @@
+
 from django.http import HttpResponse
 
 def f1(request):
@@ -6,6 +7,5 @@ def f1(request):
 def f2(request):
   return HttpResponse('<h1>Welcome to Django Class - II</h1>')
 
-def f3request):
+def f3(request):
   return HttpResponse('<p>Welcome to Django Class - III</p>')
-
